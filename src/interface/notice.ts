f31dd4266f60b10d3a5405af24649d2025632757
alfonsoHR98 <alfonsohr_98@outlook.com>
2024-10-05 21:59:28 -0600

    Se definieron los TYPES de la api

---
+++ src/interface/notice.ts
@@ -0,0 +1,21 @@
+type Notice = {
+  status: string;
+  totalResults: number;
+  articles: Article[];
+}
+
+type Article = {
+  source: Source;
+  author: string | null;
+  title: string;
+  description: string | null;
+  url: string;
+  urlToImage: string | null;
+  publishedAt: string;
+  content: string | null;
+};
+
+type Source = {
+  id: string | null;
+  name: string;
+};
