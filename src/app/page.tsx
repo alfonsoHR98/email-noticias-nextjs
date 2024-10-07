69d0c9851c63bf72ef1def2f8992a41b5894d8a5
alfonsoHR98 <alfonsohr_98@outlook.com>
2024-10-07 00:41:04 -0600

    falta la configuración, las opciones de cada tipo de petición

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,33 +8,75 @@ import {
   PopoverTrigger,
   PopoverContent,
   Button,
+  Switch,
+  Tooltip,
 } from "@nextui-org/react";
+import { div } from "framer-motion/client";
 
 export default function Home() {
   const data = useSession();
   console.log(data);
+  const [isSelected, setIsSelected] = React.useState(true);
+  const [config, setConfig] = React.useState({
+    from: "",
+    to: "",
+    languaje: "",
+    order: "",
+  });
 
   return (
     <div className="w-full h-full flex flex-col">
-      <nav className="w-full flex items-center justify-between bg-neutral-900 px-4 py-4">
+      <nav className="w-full flex items-center justify-between bg-neutral-900 px-4 py-4 shadow-lg">
         <div className="flex gap-2 text-xl">
           <h1 className="font-extrabold">Niuko</h1>
           <span>Noticias</span>
         </div>
 
         <div className="w-1/2 flex justify-center items-center gap-2">
+          <Tooltip
+            showArrow
+            placement="bottom"
+            color="default"
+            className="dark w-[300px] "
+            content={
+              <div className="flex flex-col gap-2">
+                <div>
+                  <h4 className="text-lg font-bold">Todo</h4>
+                  <p className="text-neutral-400">
+                    Busque entre millones de artículos de más de 150.000 fuentes
+                    de noticias y blogs grandes y pequeños. Este punto final se
+                    adapta al descubrimiento y análisis de artículos.
+                  </p>
+                </div>
+                <div>
+                  <h4 className="text-lg font-bold">Titulares</h4>
+                  <p className="text-neutral-400">
+                    Este punto final proporciona titulares en vivo y de última
+                    hora para un país, categoría específica en un país, fuente
+                    única o fuentes múltiples. También puedes buscar con
+                    palabras clave. Los artículos se ordenan por la fecha más
+                    temprana de publicación. Este punto final es ideal para
+                    recuperar titulares para usarlos con tickers de noticias o
+                    similares.
+                  </p>
+                </div>
+              </div>
+            }
+          >
+            <Switch
+              isSelected={isSelected}
+              onValueChange={setIsSelected}
+              className="dark"
+              color="default"
+            ></Switch>
+          </Tooltip>
           <Input
             className="dark"
-            label="Buscar"
+            label={isSelected ? "Todo" : "Titulares"}
             placeholder="Escribe palabras clave..."
             endContent={<span>🔍</span>}
           />
-          <Button
-            isIconOnly
-            color="default"
-            variant="flat"
-            className="dark"
-          >
+          <Button isIconOnly color="default" variant="flat" className="dark">
             ⚙️
           </Button>
         </div>
@@ -52,11 +94,9 @@ export default function Home() {
             />
           </PopoverTrigger>
           <PopoverContent className="w-full">
-            <Button
-              endContent={<span>🚪</span>}
-              color="danger"
-              variant="light"
-            >Cerrar Sesión</Button>
+            <Button endContent={<span>🚪</span>} color="danger" variant="light">
+              Cerrar Sesión
+            </Button>
           </PopoverContent>
         </Popover>
       </nav>
---
+++ src/interface/config.ts
@@ -0,0 +1,55 @@
+export const languaje = [
+  { code: 'ar', name: 'Árabe' },
+  { code: 'de', name: 'Alemán' },
+  { code: 'en', name: 'Inglés' },
+  { code: 'es', name: 'Español' },
+  { code: 'fr', name: 'Francés' },
+  { code: 'he', name: 'Hebreo' },
+  { code: 'it', name: 'Italiano' },
+  { code: 'nl', name: 'Holandés' },
+  { code: 'no', name: 'Noruego' },
+  { code: 'pt', name: 'Portugués' },
+  { code: 'ru', name: 'Ruso' },
+  { code: 'sv', name: 'Sueco' },
+  { code: 'ud', name: 'Urdu' },
+  { code: 'zh', name: 'Chino' }
+];
+
+export const sortByOptions = [
+  { code: 'relevancy', name: 'Relevancia' },
+  { code: 'popularity', name: 'Popularidad' },
+  { code: 'publishedAt', name: 'Fecha de Publicación' }
+];
+
+export const categoryOptions = [
+  { code: 'business', name: 'Negocios' },
+  { code: 'entertainment', name: 'Entretenimiento' },
+  { code: 'general', name: 'General' },
+  { code: 'health', name: 'Salud' },
+  { code: 'science', name: 'Ciencia' },
+  { code: 'sports', name: 'Deportes' },
+  { code: 'technology', name: 'Tecnología' }
+];
+
+export const newsTypeOptions = [
+  { code: 'everything', name: 'Todo' },
+  { code: 'top-headlines', name: 'Titulares' }
+];
+
+export const countryOptions = [
+  { code: 'ar', name: 'Argentina' },
+  { code: 'au', name: 'Australia' },
+  { code: 'br', name: 'Brasil' },
+  { code: 'ca', name: 'Canadá' },
+  { code: 'cn', name: 'China' },
+  { code: 'de', name: 'Alemania' },
+  { code: 'es', name: 'España' },
+  { code: 'fr', name: 'Francia' },
+  { code: 'gb', name: 'Reino Unido' },
+  { code: 'in', name: 'India' },
+  { code: 'it', name: 'Italia' },
+  { code: 'jp', name: 'Japón' },
+  { code: 'mx', name: 'México' },
+  { code: 'ru', name: 'Rusia' },
+  { code: 'us', name: 'Estados Unidos' }
+];
